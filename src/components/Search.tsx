ac79e0ddc21ae0d9a84476bd266414b680284809
Fernando Lara <fmaia8@hotmail.com>
2023-07-05 19:11:01 -0300

    req 4

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,12 +1,28 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { getCategories } from '../services/api';
+
+type CategoryType = {
+  id: string;
+  name: string;
+};
 
 function Search() {
   const [productValue, setProductValue] = useState('');
-  const [stateList, setStateList] = useState([]);
+  const [stateList, setStateList] = useState<CategoryType[]>([]);
+
+  useEffect(() => {
+    async function getAPI() {
+      const api = await getCategories();
+      setStateList(api);
+      console.log(stateList);
+    }
+    getAPI();
+  }, []);
 
   const handleInput = () => {
 
   };
+
   return (
     <div>
       <form>
@@ -23,6 +39,15 @@ function Search() {
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
       )}
+      {stateList.length > 0 && (
+        stateList.map((list) => {
+          return (
+            <button data-testid="category" key={ list.id }>{list.name}</button>
+
+          );
+        })
+      )}
+
     </div>
 
   );
