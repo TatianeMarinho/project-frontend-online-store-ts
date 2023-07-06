c2216a95e4b79a6fd95c7d275c8127de9a6cddac
TatianeMarinho <devtatianemarinho@gmail.com>
2023-07-05 20:27:34 -0400

    fazendo a 5

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,23 +1,40 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { getProductsFromCategoryAndQuery } from '../services/api';
 
 function Search() {
-  const [productValue, setProductValue] = useState('');
+  const [productValue, setProductValue] = useState({ search: '' });
   const [stateList, setStateList] = useState([]);
 
-  const handleInput = () => {
-
+  const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setProductValue({ ...productValue, [name]: value });
   };
+  console.log(productValue);
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const products = await getProductsFromCategoryAndQuery('', productValue.search);
+    setStateList(products.results);
+  };
+  console.log(stateList);
+
   return (
     <div>
       <Link data-testid="shopping-cart-button" to="/shoppingcart">Carrinho</Link>
-      <form>
+      <form onSubmit={ handleSubmit }>
         <input
           type="text"
           name="search"
-          value={ productValue }
+          value={ productValue.search }
+          onChange={ handleInput }
+          data-testid="query-input"
         />
-        <button onClick={ handleInput }> Buscar </button>
+        <button
+          data-testid="query-button"
+        >
+          Buscar
+        </button>
       </form>
 
       {stateList.length === 0 && (
