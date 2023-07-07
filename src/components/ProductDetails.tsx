14a7cf2a14778df2c33ba50264821a466391e4cb
Thiago Carpinetti <thiac12@gmail.com>
2023-07-07 17:07:14 -0300

    DatatestID implementados, passando nos testes

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { getProductById } from '../services/api';
 
@@ -6,11 +6,13 @@ const INITIAL_STATE = {
   title: '',
   pictures: [{ url: '' }],
   attributes: [{ id: '', name: '', value_name: '' }],
+  price: 0,
 };
 
 function ProductDetails() {
   const { id } = useParams();
   const [product, setProduct] = useState(INITIAL_STATE);
+  console.log(product);
 
   useEffect(() => {
     const fetchDetails = async () => {
@@ -22,16 +24,26 @@ function ProductDetails() {
 
   return (
     <main>
+      <div>
+        <Link to="/shoppingcart" data-testid="shopping-cart-button">
+          Carrinho
+        </Link>
+      </div>
       <div className="productDetailsContainer">
-        <h2>{product.title}</h2>
+        <h2 data-testid="product-detail-name">{product.title}</h2>
+        <h3 data-testid="product-detail-price">{`R$: ${product.price.toFixed(2)}`}</h3>
         <div>
-          <img src={ product.pictures[0].url } alt="imagem do produto" />
+          <img
+            src={ product.pictures[0].url }
+            alt="imagem do produto"
+            data-testid="product-detail-image"
+          />
           <ul>
-            {product.attributes.map((att) => (
+            {product.attributes.map((att) => (att.value_name !== null ? (
               <li key={ att.id }>
                 <p>{`${att.name}: ${att.value_name}`}</p>
               </li>
-            ))}
+            ) : null))}
           </ul>
         </div>
       </div>
